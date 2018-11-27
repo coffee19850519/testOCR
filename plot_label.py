1c1f8256adcbbf15e4cf3bd89b65f7afce6f2c14
{coffee19850519} <{14753152@qq.com}>
2018-11-26 23:00:29 -0600

    change all private variables into public

---
+++ plot_label.py
@@ -0,0 +1,40 @@
+import cv2,os
+#import matplotlib.pyplot as plt
+#import numpy as np
+
+def image_label_generator(file_path):
+    for file in os.listdir(file_path):
+      _,file_name = os.path.split(file)
+      file_name,file_ext = os.path.splitext(file_name)
+      if file_ext in ['.jpg','.jepg','.tiff','.png']:
+        #img_fp = open(file_fold+file_name+file_ext,'r')
+        label_fp = open(os.path.join(file_path,'object_'+file_name),'r')
+        label_fp.seek(0)
+        yield cv2.imread(os.path.join(file_path, file_name+file_ext)),label_fp.readlines(), \
+              os.path.join(file_path,'marked_'+file_name+file_ext)
+        label_fp.close()
+
+
+if __name__== "__main__":
+    for img,labels,result_file_name in image_label_generator(
+        r'C:\Users\coffe\Desktop\training_data'):
+      for label_txt in labels:
+        shape, coord = label_txt.split('\t')
+        # capture its coordinate points
+        coords = list(map(int, coord.split(',')))
+        if shape != 'circle':
+          #draw rectangle
+          cv2.rectangle(img,(coords[0],coords[1]),(coords[2],coords[3]),(0,
+                                                                         255,
+                                                                         0),2)
+        else:
+          #draw circle
+          cv2.circle(img,(coords[0],coords[1]),coords[2],(0,
+                                                                         255,
+                                                                         0),2)
+
+        del shape,coord
+      #save marked result
+      cv2.imwrite(result_file_name,img)
+      #release memory
+      del img,label_txt,result_file_name
