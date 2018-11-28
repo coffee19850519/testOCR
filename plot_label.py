002d038e3474d5a587a2bc3424a0f154359de5bd
{coffee19850519} <{14753152@qq.com}>
2018-11-27 19:53:38 -0600

    uniform circle's label to rectangle format

--- plot_label.py
+++ plot_label.py
@@ -14,6 +14,28 @@ def image_label_generator(file_path):
               os.path.join(file_path,'marked_'+file_name+file_ext)
         label_fp.close()
 
+def uniform_labels(file_path):
+  for file in os.listdir(file_path):
+    _, file_name = os.path.split(file)
+    file_name, file_ext = os.path.splitext(file_name)
+    if file_ext is  '.txt':
+      new_labels = []
+      # img_fp = open(file_fold+file_name+file_ext,'r')
+      label_fp = open(os.path.join(file_path, 'object_' + file_name), 'rw')
+      label_fp.seek(0)
+      for line in label_fp.readlines():
+        shape, coord = line.split('\t')
+        if shape != 'rect':
+          # capture its coordinate points
+          coords = list(map(int, coord.split(',')))
+          new_labels.append('circle '+str(coords[0]-coords[2])+','+str(coords[
+                                                               1]-coords[
+            2])+','+str(coords[0]+coords[2])+','+str(coords[1]+coords[2]))
+      label_fp.seek(0)
+      label_fp.writelines(new_labels)
+      label_fp.close()
+      del new_labels
+
 
 if __name__== "__main__":
     for img,labels,result_file_name in image_label_generator(
